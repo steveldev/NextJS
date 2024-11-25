f0f01d8a5e0bdcc2265e8253d177fb76603f8d46
Steve L. <59241607+steveldev@users.noreply.github.com>
2024-11-25 23:12:52 +0100

    Create server.js

---
+++ Deploy/O2Switch-CPanel/server.js
@@ -0,0 +1,35 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const hostname = 'localhost';
+const port = process.env.PORT || 3000;
+
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url, true);
+      const { pathname, query } = parsedUrl;
+      await handle(req, res, parsedUrl);      
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err);
+      res.statusCode = 500;
+      res.end('internal server error');
+    }
+  });
+
+  if (typeof PhusionPassenger !== 'undefined') {
+    server.listen('passenger', () => {
+      console.log('> Ready on Passenger');
+    });
+  } else {
+    server.listen(port, hostname, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
+  }
+});
